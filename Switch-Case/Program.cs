12fd3c485016ffa668d4679dd2951a201c5fca7a
FatmaBULUT <blutfatmatugce@gmail.com>
2024-08-07 19:10:13 +0300

    Switch-Case

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,23 @@
+﻿// Swich- Case
+
+int month = DateTime.Now.Month;
+
+// Expression
+switch (month)
+{
+    case 1:
+        Console.WriteLine("Ocak ayındasınız.");
+        break;
+    case 2 :
+        Console.WriteLine("Şubat ayındasınız.");
+        break;
+    case 6:
+    case 7:
+    Console.WriteLine("YAZ MEVSİMİNDESİNİZ");
+    case 8:
+        Console.WriteLine("Ağustos ayındasınız.");
+        break;
+    default:
+        Console.WriteLine("Yanlış veri girişi.");
+    break;
+}
