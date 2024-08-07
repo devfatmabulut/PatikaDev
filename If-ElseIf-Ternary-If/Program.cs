9f252a6cf0b2dde4f13a30c08e33faef80e635b5
FatmaBULUT <blutfatmatugce@gmail.com>
2024-08-07 18:12:31 +0300

    If-ElseIf-Ternary-If

---
+++ If-ElseIf-Ternary-If/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int time = DateTime.Now.Hour;
+
+if(time<=6 && time < 11)
+{
+   Console.WriteLine("Günaydın! "); 
+}
+else if(time <= 18)
+{
+    Console.WriteLine("İyi Günler!");
+}
+else
+{
+    Console.WriteLine( "İyi Geceler")
+}
+
+// ternary if -> tek satır if 
+// ? ise
+// : değilse 
+
+string sonuc = time<=18 ? "iyi günler" : "iyi geceler";
+Console.WriteLine(sonuc);
+
+// Console.WriteLine(string sonuc = time<=18 ? "iyi günler" : "iyi geceler";);
