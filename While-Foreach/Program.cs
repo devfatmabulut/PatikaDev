042122fe6de15ee96457a071171be14ce431fd17
FatmaBULUT <blutfatmatugce@gmail.com>
2024-08-07 22:39:08 +0300

    While - Foreach

---
+++ While-Foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿// While-Foreach 
+
+// 1 den başlayarak girdiğimiz sayıya kadar sayı dahil ortalama hesaplama
+
+Console.WriteLine("Bir sayı giriniz  :");
+int sayi = Convert.ToInt32(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+ while(sayac <= sayi) // koşul true oldukç çalışır
+ {
+    toplam += sayac;
+    sayac ++; // eklemezsek sonsuz döner 
+
+ }
+ Console.WriteLine(toplam/sayi);
+
+ // 'a' dan 'z' ye kadar tüm harfleri consoele yazdır
+
+char caracter = 'a';
+while (caracter < 'z')
+{
+   Console.Write(caracter);
+   caracter ++;
+}
+
+Console.WriteLine(caracter); // output : abcdefghijklmnopqrstuvwxyz
+
+// Foreach 
+
+string [] arabalar = {"bmw", "ford","toyota", "nissan"};
+
+foreach ( var araba in arabalar)
+{
+   Console.WriteLine(araba); // output : bmw
+                                      // ford
+                                      // toyota
+                                      // nissan
+} 
