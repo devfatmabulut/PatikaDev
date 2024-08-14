b5e505c5ae741509d2da14303f2146337cd6f3e9
FatmaBULUT <blutfatmatugce@gmail.com>
2024-08-14 21:21:41 +0300

    odev 2 eklendi

---
+++ C-Sharp101-Notlar/Ödev2/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. 
+(ArrayList sınıfını kullanara yazınız.) + 
+
+Negatif ve numeric olmayan girişleri engelleyin.
+Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.*/
+
