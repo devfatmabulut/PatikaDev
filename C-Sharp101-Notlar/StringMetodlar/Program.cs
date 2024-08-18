d89ffc823f6c8a593587dfae778837ad8c90d96f
FatmaBULUT <blutfatmatugce@gmail.com>
2024-08-19 01:02:32 +0300

    string Metodları eklendi

--- C-Sharp101-Notlar/StringMetodlar/Program.cs
+++ C-Sharp101-Notlar/StringMetodlar/Program.cs
@@ -33,5 +33,30 @@ System.Console.WriteLine(degisken.StartsWith("Merhaba")); // ... başlıyor mu ?
 //IndexOf -> İlk bulduğu yerdeki ilk indexi verir bulamazsa -1 döner
 System.Console.WriteLine(degisken.IndexOf("CS"));
 System.Console.WriteLine(degisken.IndexOf("f"));
+System.Console.WriteLine(degisken.LastIndexOf("i")); // son indexi
 
 // Insert
+System.Console.WriteLine(degisken.Insert(0,"Merhaba! "));
+
+//PadLeft ,PadRight
+System.Console.WriteLine(degisken+degisken2.PadLeft(30)); // değişken2 nin sonuna uznuluğu 30 olacak kadar boşluk ekler
+System.Console.WriteLine(degisken.PadRight(50)+degisken2); // değişken1 in sonuna uznuluğu 50 olacak kadar  boşluk ekler
+System.Console.WriteLine(degisken.PadRight(50)+degisken2.PadLeft(30,'*')); 
+
+// Remove
+// silme
+System.Console.WriteLine(degisken.Remove(10)); // 10.karakterden sonrasını siler
+System.Console.WriteLine(degisken.Remove(5,3)); // 5 den başlar 3 karakter siler.
+System.Console.WriteLine(degisken.Remove(0,1)); // 0 dan başlar  1 karakter siler. ilk harfi
+
+// Replace
+System.Console.WriteLine(degisken.Replace("CSharp","C#")); // değiştirme
+System.Console.WriteLine(degisken.Replace(" ","*")); // değiştirme
+
+// Split** 
+System.Console.WriteLine(degisken.Split(' ')[1]); // boşluklara göre parçala bir diziye ata bana 1. indexteki veriyi bana getir
+
+// Sunstring
+System.Console.WriteLine(degisken.Substring(4)); // 4. indexten başlar sonuna kadar getirir.
+System.Console.WriteLine(degisken.Substring(4,6)); // 4. indexten başlar 6 karakter getir.
+
